897ba5af39f8d5b85847154c0a8667fdd802c245
bsaoptima <BSA040@student.bham.ac.uk>
2023-02-08 10:01:26 +0000

    ok

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
+import { NewFileLocation } from './enums';
+import { path } from './utils';
 // Remember to rename these classes and interfaces!
 
 
@@ -25,6 +26,42 @@ export default class MyPlugin extends Plugin {
 
 	}
 
+	
+	async createNewNote(input: string): Promise<void> {
+		const { vault } = this.app;
+		const { adapter } = vault;
+		const prependDirInput = path.join(this.newDirectoryPath, input);
+		const { dir, name } = path.parse(prependDirInput);
+		const directoryPath = path.join(this.folder.path, dir);
+		const filePath = path.join(directoryPath, `${name}.md`);
+
+		try {
+		const fileExists = await adapter.exists(filePath);
+		if (fileExists) {
+			// If the file already exists, respond with error
+			throw new Error(`${filePath} already exists`);
+		}
+		if (dir !== '') {
+			// If `input` includes a directory part, create it
+			await this.createDirectory(dir);
+		}
+		const File = await vault.create(filePath, '');
+		// Create the file and open it in the active leaf
+		let leaf = this.app.workspace.getLeaf(false);
+		if (this.mode === NewFileLocation.NewPane) {
+			leaf = this.app.workspace.splitLeafOrActive();
+		} else if (this.mode === NewFileLocation.NewTab) {
+			leaf = this.app.workspace.getLeaf(true);
+		} else if (!leaf) {
+			// default for active pane
+			leaf = this.app.workspace.getLeaf(true);
+		}
+		await leaf.openFile(File);
+		} catch (error) {
+		new Notice(error.toString());
+		}
+	}
+	}
 
 	onunload() {
 
@@ -32,39 +69,3 @@ export default class MyPlugin extends Plugin {
 
 }
 
-
-async createNewNote(input: string): Promise<void> {
-    const { vault } = this.app;
-    const { adapter } = vault;
-    const prependDirInput = path.join(this.newDirectoryPath, input);
-    const { dir, name } = path.parse(prependDirInput);
-    const directoryPath = path.join(this.folder.path, dir);
-    const filePath = path.join(directoryPath, `${name}.md`);
-
-    try {
-      const fileExists = await adapter.exists(filePath);
-      if (fileExists) {
-        // If the file already exists, respond with error
-        throw new Error(`${filePath} already exists`);
-      }
-      if (dir !== '') {
-        // If `input` includes a directory part, create it
-        await this.createDirectory(dir);
-      }
-      const File = await vault.create(filePath, '');
-      // Create the file and open it in the active leaf
-      let leaf = this.app.workspace.getLeaf(false);
-      if (this.mode === NewFileLocation.NewPane) {
-        leaf = this.app.workspace.splitLeafOrActive();
-      } else if (this.mode === NewFileLocation.NewTab) {
-        leaf = this.app.workspace.getLeaf(true);
-      } else if (!leaf) {
-        // default for active pane
-        leaf = this.app.workspace.getLeaf(true);
-      }
-      await leaf.openFile(File);
-    } catch (error) {
-      new Notice(error.toString());
-    }
-  }
-}
