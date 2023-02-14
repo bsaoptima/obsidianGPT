72f45b27dc58c7d3b82672fc60789371dba87dfa
bsaoptima <BSA040@student.bham.ac.uk>
2023-02-14 17:30:00 +0000

    for some reason runs two prompts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 import { CreateCompletionResponseChoicesInner, Configuration, OpenAIApi } from 'openai';
 import { MyPluginSettingsTab } from 'settings';
 
@@ -13,20 +13,51 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 };
 
 
-
 export default class MyPlugin extends Plugin {
   openai: OpenAIApi;
   settings: MyPluginSettings;
 
   async onload() {
     await this.loadSettings();
-
     this.addSettingTab(new MyPluginSettingsTab(this.app, this));
+
+    this.addCommand({
+      id: 'find-chapters',
+      name: 'Find Topic Chapters',
+      callback: async () => {
+        const activeMarkdown_name = this.app.workspace.getActiveFile()?.basename;
+        const activeMarkdown = this.app.workspace.getActiveFile()
+        new Notice(`${activeMarkdown_name}`)
+        if (activeMarkdown_name){
+          const fileContent = await this.FindChapters(activeMarkdown_name);
+
+          if (activeMarkdown instanceof TFile){
+            await this.app.vault.modify(activeMarkdown, fileContent);
+          }
+        }
+      }
+    });
 
     this.addCommand({
       id: 'openai-prompt',
       name: 'OpenAI Prompt',
-      callback: this.onOpenAIPrompt.bind(this),
+      callback: () =>{
+        new OpenAIPrompt(this.app, this.openai).open();
+      }
+    });
+    
+    this.addCommand({
+      id: "create-pages",
+      name: "Create Pages with List",
+      callback: async () =>{
+        const activeFileName = this.app.workspace.getActiveFile();
+        if (activeFileName instanceof TFile){
+          const content = await this.app.vault.read(activeFileName);
+          const modifiedContent = this.modifyFileContent(content);
+          new Notice(`${modifiedContent}`)
+          await this.app.vault.modify(activeFileName, modifiedContent)
+        }
+      }
     });
 
     this.openai = new OpenAIApi(
@@ -36,12 +67,23 @@ export default class MyPlugin extends Plugin {
     );
   }
 
-  onOpenAIPrompt(){
-    new OpenAIPrompt(this.app, this.openai).open();
-  }
-
   onunload() {
     console.log('unloading plugin');
+  }
+
+  async FindChapters(topic: string) {
+    const completion = await this.openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Answer the following question: What are the main chapters of "+ topic + "?{}",
+      temperature: .7,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop: ['{}'],
+    });
+
+    return completion?.data?.choices?.[0]?.text?.trim() || '';
   }
 
   async loadSettings() {
@@ -51,6 +93,21 @@ export default class MyPlugin extends Plugin {
   async saveSettings() {
     await this.saveData(this.settings);
   }
+
+  private modifyFileContent(content: string): string{
+    const lines = content.split('\n');
+    const regex = /^\d+\.\s+/;
+
+    const result = lines
+      .filter(line => regex.test(line))
+      .map(line => line.replace(regex, ''))
+      .join('\n');
+    
+    const listArray = result.split('\n');
+    const modifiedList = listArray.map((item) =>`[[${item.trim()}]]`);
+    return modifiedList.join('\n');
+  }
+
 }
 
 class OpenAIPrompt extends Modal {
@@ -65,7 +122,7 @@ class OpenAIPrompt extends Modal {
 
     contentEl.createEl('h2', { text: 'OpenAI Prompt' });
 
-    new Notice('Please enter a topic.');
+    new Notice('Enter a prompt.');
 
     const formEl = contentEl.createEl('form');
 
@@ -75,14 +132,30 @@ class OpenAIPrompt extends Modal {
       const topic = this.topicInput.value;
 
       if (!topic) {
-        new Notice('Please enter a topic.');
+        new Notice('Please enter a prompt.');
         return;
       }
+      const file = this.app.workspace.getActiveViewOfType(MarkdownView);
+      if (!file) return;
 
-      const fileName = `${topic}.md`;
-      const fileContent = await this.runCompletion(topic);
-      await this.app.vault.create(fileName, fileContent);
-      new Notice('File created!');
+      const editor = file.editor;
+      const cursor = editor.getCursor();
+
+      if (!cursor) return;
+
+      const line = cursor?.line;
+      const ch = cursor?.ch;
+      const position = editor.posToOffset({ line, ch });
+
+      const activeFile = this.app.workspace.getActiveFile();
+      if (!activeFile) return;
+
+      const content = await this.app.vault.read(activeFile);
+      const newContent = await this.promptGPT(topic);
+
+      const insertedContent = this.insertTextAtPosition(content, `- ${newContent}`, position)
+      await this.app.vault.modify(activeFile, insertedContent);
+      new Notice('File modified!');
       this.close();
     });
 
@@ -94,6 +167,7 @@ class OpenAIPrompt extends Modal {
 
     submitButton.addEventListener('click', () => {
       formEl.dispatchEvent(new Event('submit'));
+    
     });
   }
 
@@ -102,10 +176,10 @@ class OpenAIPrompt extends Modal {
     contentEl.empty();
   }
 
-  async runCompletion(topic: string) {
-    const completion = await this.openai.createCompletion({
+  async promptGPT(prompt: string){
+    const output = await this.openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Answer the following question: What are the main chapters of "+ topic + "?{}",
+      prompt: prompt + "{}",
       temperature: .7,
       max_tokens: 150,
       top_p: 1,
@@ -114,12 +188,11 @@ class OpenAIPrompt extends Modal {
       stop: ['{}'],
     });
 
-    return completion?.data?.choices?.[0]?.text?.trim() || '';
+    return output.data.choices[0].text?.trim().replace(/\n/g, "\n-");
   }
+
+  private insertTextAtPosition(text:string, insert: string, position: number){
+    return text.slice(0, position) + insert + text.slice(position);
+  }
+  
 }
-
-
-
-
-
-
